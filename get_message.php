3c245db804771ce4534a774e4e77c575338d42a6
{takumi-matsuura} <takumi_matsuura@rudel.jp>
2020-04-02 18:01:19 +0900

    [feat]ブラウザ上でメッセージを送信

---
+++ get_message.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>ゲットするやつ</title>
+    </head>
+    <body>
+    <?php
+        echo $_POST["post_message"];
+    ?>
+    </body>
+</html>
---
+++ post_form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>ポストするやつ</title>
+    </head>
+    <body>
+        <form method="post" action="get_message.php">
+            <label>入力：</label>
+            <input type="text" name="post message">
+            <input type="submit" value="送信">
+        </form>
+    </body>
+</html>
