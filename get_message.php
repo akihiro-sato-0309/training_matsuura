aa1903af22e054432d2d0cffa05ef83d4af62abb
{takumi-matsuura} <takumi_matsuura@rudel.jp>
2020-04-02 18:59:56 +0900

    [feat]データベースと接続し、データを受信する

--- get_message.php
+++ get_message.php
@@ -6,7 +6,27 @@
     </head>
     <body>
     <?php
-        echo $_POST["post_message"];
+        $message = $_POST["post_message"];
+        echo $message;
+        echo '<br>';
+
+        $dsn = 'mysql:dbname=training_matsuura;host=localhost';
+        $user = 'training';
+        $password = 'trainingtest';
+
+    try {
+        $dbh = new PDO($dsn, $user, $password);
+        $sql = 'select * from Foundation';
+
+        $result = "INSERT INTO foundation (message) VALUES ('', ''$message')";
+
+        foreach ($dbh->query($sql) as $row) {
+            echo $row['message'];
+            echo '<br>';
+        }
+    } catch (PDOException $e) {
+            echo $e -> getMessage();
+    }
     ?>
     </body>
 </html>
