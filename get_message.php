995ef96b107fa364876e069934657dde71d37902
{takumi-matsuura} <takumi_matsuura@rudel.jp>
2020-04-03 10:58:40 +0900

    [feat]データベースへデータを送信する処理を追加

--- get_message.php
+++ get_message.php
@@ -8,6 +8,8 @@
     <?php
         $message = $_POST["post_message"];
         echo $message;
+        echo '<br><br>';
+        echo 'ここから下DB';
         echo '<br>';
 
         $dsn = 'mysql:dbname=training_matsuura;host=localhost';
@@ -18,7 +20,10 @@
         $dbh = new PDO($dsn, $user, $password);
         $sql = 'select * from Foundation';
 
-        $result = "INSERT INTO foundation (message) VALUES ('', ''$message')";
+        $result = "INSERT INTO foundation (message) VALUES (:message)";
+        $stmt = $dbh -> prepare($result);
+        $params = array(':message' => $message);
+        $stmt -> execute($params);
 
         foreach ($dbh->query($sql) as $row) {
             echo $row['message'];
